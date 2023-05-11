0b7ebe53a837b0f1505113d4f9d1fbdd22a15bd3
PauliMorales <paumor271@gmail.com>
2023-05-11 08:22:51 -0500

    update css

--- src/components/Purchases/ProductPurchases.jsx
+++ src/components/Purchases/ProductPurchases.jsx
@@ -4,7 +4,7 @@ const ProductPurchases = ({ prodPurchases }) => {
   return (
     <article className="prodpurchases__container">
       <img className="prodpurchases__img f-grow" src={prodPurchases?.product.images[0].url} alt="" />
-      <h3 className="prodpurchases__title f-grow">{prodPurchases.product.title}</h3>
+      <h3 className="prodpurchases__prod f-grow">{prodPurchases.product.title}</h3>
       <span className="prodpurchases__unid f-grow">{prodPurchases.quantity}</span>
       <span className="prodpurchases__price f-grow">${prodPurchases.quantity * prodPurchases.product.price}</span>
     </article>
