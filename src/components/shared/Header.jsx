6135cb9352dcb806e16519154bca69cad50a86ec
PauliMorales <paumor271@gmail.com>
2023-05-11 14:31:35 -0500

    create log-out

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,43 +1,52 @@
-
-import { Link } from 'react-router-dom'
-import './Styles/Header.css'
+import { Link, useNavigate } from "react-router-dom";
+import "./Styles/Header.css";
 
 const Header = () => {
+  const navigate = useNavigate();
 
-  
+  const logout = () => {
+    localStorage.removeItem("token");
+    navigate("/login");
+  };
   return (
     <header className="header__container">
-    <h1 className="header__logo">
-      <Link to="/" className="header__title">
-        e-commerce
-      </Link>
-    </h1>
-    <nav className="header__nav">
-      <ul className="header__list">
-        <li className="header__items">
-          <Link to="/login">
-            <i className="bx bx-user"></i>
-          </Link>
-        </li>
-        <li className="header__items">
-          <Link to="/register">
-            <i className="bx bx-user-plus"></i>
-          </Link>
-        </li>
-        <li className="header__items">
-          <Link to="/purchases">
-            <i className="bx bx-shopping-bag"></i>
-          </Link>
-        </li>
-        <li className="header__items ">
-          <Link to="/cart">
-            <i className="bx bx-cart"></i>
-          </Link>
-        </li>
-      </ul>
-    </nav>
-  </header>
-  )
-}
+      <h1 className="header__logo">
+        <Link to="/" className="header__title">
+          e-commerce
+        </Link>
+      </h1>
+      <nav className="header__nav">
+        <ul className="header__list">
+          <li className="header__items">
+            <Link to="/login">
+              <i className="bx bx-user"></i>
+            </Link>
+          </li>
+          <li className="header__items">
+            <Link to="/register">
+              <i className="bx bx-user-plus"></i>
+            </Link>
+          </li>
+          <li className="header__items">
+            <Link to="/purchases">
+              <i className="bx bx-shopping-bag"></i>
+            </Link>
+          </li>
+          <li className="header__items ">
+            <Link to="/cart">
+              <i className="bx bx-cart"></i>
+            </Link>
+          </li>
+          <li className="header__items">
+            <i
+              className="bx bx-log-out header__log-out"
+              onClick={() => logout()}
+            ></i>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
