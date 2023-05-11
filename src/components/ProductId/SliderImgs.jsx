44f643c3dcf170dd4a0fe3211851fbc68fd684ff
DEZACATOAcademlo <frank2capillo@gmail.com>
2023-05-10 20:04:53 -0500

    versionpeter1xd

--- src/components/ProductId/SliderImgs.jsx
+++ src/components/ProductId/SliderImgs.jsx
@@ -1,12 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './Styles/SliderImgs.css'
 
 const SliderImgs = ({product}) => {
 
-    
+  const [numberImg, setNumberImg] = useState(0)
+
+    const objStyle = {
+      transform: `translateX(calc(-${numberImg}/3 * 100%))`
+    }
+    const handlePrev = () => {
+      if(numberImg - 1 < 0){
+        setNumberImg(2)
+        } else {
+          setNumberImg(numberImg - 1)
+        }
+      }
+  
+    const handleNext = () => {
+      if(numberImg + 1> 2){
+          setNumberImg(0)
+        } else {
+          setNumberImg(numberImg + 1)
+        }
+      }
 
   return (
-    <div>
-        <img src={product?.images[0].url} alt="" />
+    <div className='slider'>
+      <button onClick={handlePrev}  className='slider__arrowhead slider__left'>
+        &lt;</button>
+      <div style={objStyle} className='slider__interior'>
+      {
+        product?.images.map((imgInfo) => (
+          <div key={imgInfo.id} className='slider__img-container'>
+            <img className='slider__img' 
+            src={imgInfo.url} 
+            alt="" 
+            />
+          </div>
+        ))
+      }
+      </div>
+      <button onClick={handleNext} className='slider__arrowhead slider__right'>
+        &gt;</button>
     </div>
   )
 }
